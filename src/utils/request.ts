46e45eb2cce6c5dc8513ac7c01153e9dfe196fa3
Phoenix0356 <1242780667@qq.com>
2024-06-19 20:51:57 +0800

    增加对于重登录的拦截器

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import router from "../router";
 
 const request = axios.create({
     baseURL: "/api",
@@ -9,5 +10,17 @@ request.interceptors.request.use((config) => {
     config.headers['authorization'] = localStorage.getItem('token');
     return config;
 })
+
+request.interceptors.response.use(
+    async (resp) => {
+        if(resp.data.result === 2){
+            console.log(resp.data.msg)
+            localStorage.clear()
+            sessionStorage.clear()
+            await router.push('/user/login')
+        }
+        return resp;
+    },
+)
 
 export default request
